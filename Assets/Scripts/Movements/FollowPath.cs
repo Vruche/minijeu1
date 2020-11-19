42dc02ae6bd6da23ca7919e8ea52d4ec40bbf0d3
Vruche <martin.boulanger@protonmail.com>
2020-11-19 16:42:46 +0100

    TD niveau insane

---
+++ Assets/Scripts/Movements/FollowPath.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    /*
+     * Le but de ce TD est de permettre à Maxime de faire se déplacer une mouche entre plusieurs points
+     */
+
+    // Permet de set des points dans l inspector
+    public Transform[] points;
+
+    // Index du point cible courant
+    private int index;
+
+    void Awake()
+    {
+        index = 0;
+    }
+
+    void Update()
+    {
+        // Vector3.MoveTowards ou Vector3.Smoothdamp pour lisser, proposer les 2 avec une option peut etre...
+    }
+}
