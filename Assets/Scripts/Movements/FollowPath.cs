090f68cae6e5f3b32d713be71b4899f9d12f3c87
Maxime Servais <maximeservais@laposte.net>
2020-11-20 21:06:38 +0100

    Ajout de la trajectoire de la Mouche

--- Assets/Scripts/Movements/FollowPath.cs
+++ Assets/Scripts/Movements/FollowPath.cs
@@ -10,9 +10,9 @@ public class FollowPath : MonoBehaviour
 
     // Permet de set des points dans l inspector
     public Transform[] points;
-
     // Index du point cible courant
     private int index;
+    private Vector3 deltaMove;
 
     void Awake()
     {
@@ -21,6 +21,16 @@ public class FollowPath : MonoBehaviour
 
     void Update()
     {
+        float ySin;
         // Vector3.MoveTowards ou Vector3.Smoothdamp pour lisser, proposer les 2 avec une option peut etre...
+        //Changement de target
+        if (transform.position.x.Equals(points[index].position.x))
+            index = (index + 1) % points.Length;
+
+        deltaMove = Vector3.MoveTowards(transform.position, points[index].position, 2f * Time.deltaTime);
+        ySin = Mathf.Sin(Time.time * 10) * Time.deltaTime;
+        deltaMove.y += ySin;
+
+        transform.position = deltaMove;
     }
 }
